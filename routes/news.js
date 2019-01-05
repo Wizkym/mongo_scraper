f4a2f332945c79be9491003ca39ce5a038ddf3e9
Kelvin Jones <kevinjones4453@gmail.com>
2019-01-05 09:26:46 -0500

    diagnosing app

--- routes/news.js
+++ routes/news.js
@@ -10,10 +10,7 @@ const router = express.Router();
 // Connect to the Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-    .then( () => {
-        console.log("DATABASE CONNECTED");
-    });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 router.get("/", (req,res) => {
     res.redirect("/scrape");
